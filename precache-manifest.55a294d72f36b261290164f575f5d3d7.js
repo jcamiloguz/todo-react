26c55d495e8908bc6b85ecb47b788875f29e6e47
jcamiloguz <jcamiloguz1@gmail.com>
2022-01-10 02:55:04 -0500

    Updates

---
+++ precache-manifest.55a294d72f36b261290164f575f5d3d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ac5d7c6406fea262602df162555e8f",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "d6bb5e1ce6f03d5425d1",
+    "url": "/todo-react/static/css/main.d283ff3f.chunk.css"
+  },
+  {
+    "revision": "c1c11ac080400dd5699f",
+    "url": "/todo-react/static/js/2.fb54a156.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/todo-react/static/js/2.fb54a156.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bb5e1ce6f03d5425d1",
+    "url": "/todo-react/static/js/main.786c6d83.chunk.js"
+  },
+  {
+    "revision": "f2c759d1b8da648375a9",
+    "url": "/todo-react/static/js/runtime-main.c664482e.js"
+  }
+]);
